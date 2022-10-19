1ca80e9325ccc8372590702582f9a057d1f57a4b
Deinyibo <deinyibobraide@gmail.com>
2022-10-18 21:45:42 -0700

    group work

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 /**
  * _printf - prints and input into the standard output
  * @format: the format string
- * Return: number of bytes printed
+ * Return: number of bytes printed 
  */
 
 int _printf(const char *format, ...)
--- numbers.c
+++ numbers.c
@@ -15,7 +15,7 @@ int _isdigit(int c)
  * _strlen - returns the length of a string
  * @s: the string whose length to check
  *
- * Return: integer length of string
+ * Return: integer length of string 
  */
 int _strlen(char *s)
 {
